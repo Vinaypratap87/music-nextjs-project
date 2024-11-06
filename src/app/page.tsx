8ea9fbb86b33f008761d2c8087cc0a3b34e9b7f0
Vinaypratap87 <thakurvinaypratap797@gmail.com>
2024-11-06 09:41:38 +0530

    create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import WhyChooseUs from "@/components/WhyChooseUs";
 import { main } from "framer-motion/client";
 
 export default function Home (){
   return(
     <main >
-     
     <HeroSection />
+    <FeaturedCourses/>
+    <WhyChooseUs/>
+    <MusicSchoolTestimonials/>
    </main>
   )
 }
